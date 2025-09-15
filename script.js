2e838e00f66b57e58bf843c2e816630136f42eac
pedrovinici8 <141650777+pedrovinici8@users.noreply.github.com>
2025-09-15 09:56:46 -0300

    sla

---
+++ script.js
@@ -0,0 +1,13 @@
+const caixaPrincipal = document.querySelector(caixa-principal);
+const caixaPerguntas = document.querySelector(caixa-perguntas);
+const caixaAlternativas = document.querySelector(caixa-alternativas);
+const caixaResultado = document.querySelector(caixa-resultados);
+const textoResultado = document.querySelector(texto-resultado);
+const perguntas = [
+{
+enunciado: 'pergunta 1',
+ alternativas: ('alternativa1', 'alternativa 2'), },
+{
+enunciado: 'pergunta 2', 
+alternativas: ['alternativa 1', 'alternativa 2'], }
+]
